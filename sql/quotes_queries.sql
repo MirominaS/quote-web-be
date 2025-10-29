03d24d415e24495e8fd97a9cc42afdb1a56d34d7
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-10-29 10:53:30 +0530

    queries added to the backend

---
+++ sql/quotes_queries.sql
@@ -0,0 +1,5 @@
+insert into quote_core.quotes_table(quotes, quote_author, quote_tags, created_by, deleted_by)
+VALUES ('khdsfkjhaf jdhflashdfj kshdlfhsldhfksdhf hsdfkhalskjhfsd', 'sajeethan',
+        Array['sdkjfhs', 'sjdhfkjdh'], 1, 1);
+
+select * from quote_core.quotes_table;
