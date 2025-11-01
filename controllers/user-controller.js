070d415ec674ac71e56f1785bdece2c82f2517fc
MirominaS <miromin-im19063@stu.kln.ac.lk>
2025-11-01 12:24:07 +0530

    added hashed_password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,15 @@
 import { pool } from "../config/db-config.js"
-import { errorlogger } from "../utils/loggers.js"
+import { errorlogger, logger } from "../utils/loggers.js"
+import bcrypt from 'bcrypt'
 
 
 export const insertUserController = async(req,res) => {
-    try {
-        const {username,email,hashed_password} = req.body 
+    try {       
+        const {username,email,password} = req.body 
+        const saltRounds = 12;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashed_password = await bcrypt.hash(password,salt);
+        logger("Hashed_password",hashed_password)
         await pool.query(`insert into quote_users.user_table(username,email,hashed_password) values($1,$2,$3)`,[username,email,hashed_password])
         return res.status(200).json({
             data: {username,email,hashed_password},
@@ -12,7 +17,7 @@ export const insertUserController = async(req,res) => {
             success:true
         })  
     } catch (error) {
-        errorlogger("error",error)
+        errorlogger("error:",error)
         return res.status(500).json({
             data:null,
             message:"Something went wrong!",
